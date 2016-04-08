f3936a65caad9e78365d526c1ad98b68c10e8f6f
buxizhizhou <buxizhizhou530@gmail.com>
2016-04-08 20:43:03 +0800

    the first push -mystg

---
+++ Version 4/IndoorSTG/src/indoorstg/IndoorSTG.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ * 2015-06-16 开始修改的。第一次主要GenerateData.java里判断start是否为空
+ * 06-16晚 加入Git控制
+ */
+package indoorstg;
+
+/**
+ *
+ * @author Administrator
+ */
+public class IndoorSTG {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
